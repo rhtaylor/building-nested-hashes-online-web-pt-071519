348ddf6997afa4e57889cfdf74841de0c9201b72
Randy Taylor <randyisbusy@gmail.com>
2019-07-25 14:44:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,29 @@
-def second_challenge
-  epic_tragedy = {
-    :montague => {
-      patriarch: {},
-      matriarch: {},
-      hero: {},
-
-      hero_friends: {}
+epic_tragedy = {
+ :montague => {
+    :patriarch => {
+      #your key/value pairs here
       },
-    :capulet => {
-      #your key/value pairs here!
-
-
-      }}
+    :matriarch => {
+      #your key/value pairs here
+      },
+    :hero => {
+      #your key/value pairs here
+      },
+    :hero_friends => []
+ }, 
+ :capulet => {
+    :patriarch => {
+      #your key/value pairs here
+      },
+    :matriarch => {
+      #your key/value pairs here
+      },
+    :heroine => {
+      #your key/value pairs here
+      },
+    :heroine_friends => []
+ }
+}
 
 
 end
